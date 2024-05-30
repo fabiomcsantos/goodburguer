ad6b8ec7f960b4a37c97a14da79af0e80367e8c2
FABIO MENINI CARDOSO DOS SANTOS <fabio.mcsantos@senacsp.edu.br>
2024-05-29 21:49:24 -0300

    primeiro commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+    white: "#FFFFFF",
+    black: "#000000",
+  
+    blue_100: "#1E293B",
+    blue_200: "#0F172A",
+    blue_300: "#090912",
+    
+    green_100: "#BEF264",
+  
+  }
